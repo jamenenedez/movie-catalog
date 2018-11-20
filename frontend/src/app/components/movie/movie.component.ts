c2c23dcd5bbf895f6b07737be98938f721fc6239
Jose Armando Menendez Clavijo <jdoemax88@gmail.com>
2018-11-19 20:47:33 -0800

    Adicionando movie

--- frontend/src/app/components/movie/movie.component.ts
+++ frontend/src/app/components/movie/movie.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MovieService } from '../../services/movie.service';
+import { NgForm } from '@angular/forms';
+import { Movie } from 'src/app/models/movie';
+
+declare var M: any;
+
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.getAll();
+  }
+
+  add(form: NgForm) {
+    if (form.value._id != "" && form.value._id != null) {
+      this.movieService.put(form.value).subscribe(res => {
+        this.getAll();
+        this.reset(form);
+        M.toast({ html: "Successfuly updated" });
+      });
+    }
+    else {
+      this.movieService.post(form.value).subscribe(res => {
+        this.getAll();
+        this.reset(form);
+        M.toast({ html: "Successfuly saved" });
+      });
+    }
+  }
+
+  delete(_id: String) {
+    if (confirm("Are you sure you want to delete it")) {
+      this.movieService.delete(_id).subscribe(res => {
+        this.getAll();
+        M.toast({ html: "Successfuly deleted" });
+      });
+    }
+  }
+
+  getAll() {
+    this.movieService.getAll().subscribe(res => {
+      this.movieService.movies = res as Movie[];
+    });
+  }
+
+  edit(movie: Movie) {
+    this.movieService.selected = movie;
+  /*   this.movieService.selected.nationality = movie.nationality; */
+  }
+
+  reset(form: NgForm) {
+    if (form) {
+      form.reset();
+      this.movieService.selected = new Movie();
+    }
+
   }
 
 }
